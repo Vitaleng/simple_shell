6f684ddc53237ae7ae11eab1fbbef2c1be540daf
NomfundoPM <nomfundomatete@gmail.com>
2023-09-19 07:33:29 -0700

    execute.c

--- execute.c
+++ execute.c
@@ -11,41 +11,41 @@
 */
 int execute(data_of_program *data)
 {
-    int retval = 0, status;
-    pid_t pidd;
+	int retval = 0, status;
+	pid_t pidd;
 
-    retval = builtins_list(data);
-    if (retval != -1)
-        return retval;
+	retval = builtins_list(data);
+	if (retval != -1)
+		return (retval);
 
-    retval = find_program(data);
-    if (retval)
-        return retval;
+	retval = find_program(data);
+	if (retval)
+		return (retval);
 
-    pidd = fork();
-    if (pidd == -1) 
+	pidd = fork();
+	if (pidd == -1)
 {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 
-    if (pidd == 0) 
+	if (pidd == 0)
 {
-        retval = execvp(data->tokens[0], data->tokens);
-        if (retval == -1) 
+		retval = execvp(data->tokens[0], data->tokens);
+		if (retval == -1)
 {
-            perror(data->command_name);
-            exit(EXIT_FAILURE);
-        }
-    } 
-else 
+			perror(data->command_name);
+   exit(EXIT_FAILURE);
+		}
+	}
+else
 {
-        wait(&status);
-        if (WIFEXITED(status))
-            errno = WEXITSTATUS(status);
-        else if (WIFSIGNALED(status))
-            errno = 128 + WTERMSIG(status);
-    }
+		wait(&status);
+		if (WIFEXITED(status))
+			errno = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			errno = 128 + WTERMSIG(status);
+	}
 
-    return 0;
-}
+	return (0);
+	}
