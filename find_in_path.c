3bf572b27b3b3ee0833aa52ef19a46304869a9cb
Vitaleng <frecklesineeleng@gmail.com>
2023-09-19 11:07:34 -0700

    simple shell

--- find_in_path.c
+++ find_in_path.c
@@ -4,3 +4,118 @@
 #include <string.h>
 
 /**
+ * check_file - it checks if a file exists, if it is not a directory, and
+ * if it has execution permissions
+ * @full_path: pointer to the full file name
+ * Return: zero on success, or error code if it exists
+ */
+int check_file(char *full_path)
+{
+	struct stat sb;
+
+	if (stat(full_path , &sb) != -1)
+	{
+		if (S_ISDIR(sb.st_mode) || access(full_path, X_OK))
+		{
+			errno = 126;
+			return (126);
+		}
+		return (0);
+	}
+	/* if the file doesn't exist */
+	errno = 127;
+	return (127);
+}
+
+/**
+ * find_program - finds a program in the PATH environment variable
+ * @data: a pointer to the program's data
+ * Return: zero is success, or an error code otherwise
+ */
+int find_program(data_of_program *data)
+{
+	int i = 0, ret_code = 0;
+	char **directories;
+
+	if (!data->command_name)
+		return (2);
+
+	/* if it is a full_path or an executable in the same path */
+	if (data->command_name[0] == '/' || data->command_name[0] == '.')
+		return (check_file(data->command_name));
+
+	free(data->tokens[0]);
+	data->tokens[0] = str_concat(str_duplicate("/"), data->command_name);
+	if (!data->tokens[0])
+		return (2);
+
+	directories = tokenize_path(data); /* Search in the PATH */
+
+	if (!directories || !directories[0])
+	{
+		errno = 127;
+		return (127);
+	}
+	for (i = 0; directories[i]; i++)
+	{ /* Appends the function_name to path */
+		directories[i] = str_concat(directories[i], data->tokens[0]);
+		ret_code = check_file(directories[i]);
+		if (ret_code == 0 || ret_code == 126)
+		{/* The file was found, is not a directory, and has execute permissions */
+			errno = 0;
+			free(data->tokens[0]);
+			data->tokens[0] = str_duplicate(directories[i]);
+			free_array_of_pointers(directories);
+			return (ret_code);
+		}
+	}
+	free(data->tokens[0]);
+	data->tokens[0] = NULL;
+	free_array_of_pointers(directories);
+	return (ret_code);
+}
+
+/**
+ * tokenize_path - tokenize the PATH environment variable into directories
+ * @data: a pointer to the program's data
+ * Return: array of path directories
+ */
+char **tokenize_path(data_of_program *data)
+{
+	int i = 0;
+	int counter_directories = 2;
+	char **tokens = NULL;
+	char *PATH;
+
+	/* Get the PATH value */
+	PATH = env_get_key("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{/* Path not found */
+		return (NULL);
+	}
+
+	PATH = str_duplicate(PATH);
+
+	/* Find the number of directories in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			counter_directories++;
+	}
+
+	/* Reserve space for the array of pointers */
+	tokens = malloc(sizeof(char*) *counter_directories);
+
+	/* Tokenize and duplicate each token of path */
+	i = 0;
+	tokens[i] = str_duplicate(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = str_duplicate(_strtok(NULL, ":"));
+	}
+
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+}
+
--- helpers_free.c
+++ helpers_free.c
@@ -2,3 +2,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct data_of_program;
+
+void free_recurrent_data(data_of_program *data);
+void free_all_data(data_of_program *data);
+void free_array_of_pointers(char **array);
+
+typedef struct data_of_program {
+	char **tokens;
+	char *input_line;
+	char command_name;
+	int file_descriptor;
+	char **env;
+	char **alias_list;
+	char *program_name;
+} data_of_program;
+
+/**
+ * free_recurrent_data - free the fields needed each loop
+ * @data: struct of the program's data
+ * Return: None
+ */
+void free_recurrent_data(data_of_program *data)
+{
+	if (data->tokens)
+		free_array_of_pointers(data->tokens);
+	if (data->input_line)
+		free(data->input_line);
+	if (data->command_name)
+		free(data->command_name);
+
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->tokens = NULL;
+}
+
+/**
+ * free_all_data - free all fields of the data
+ * @data: struct of the program's data
+ * Return: None
+ */
+void free_all_data(data_of_program *data)
+{
+	if (data->file_descriptor != 0)
+	{
+		if (close(data->file_descriptor))
+			perror(data->program_name);
+	}
+	free_recurrents_data(data);
+	free_array_of_pointers(data->env);
+	free_array_of_pointers(data->alias_list);
+}
+
+/**
+ * free_aray_of_pointers - frees each pointer of an array of and the
+ * array too
+ * @array: array of pointers
+ * Return: None
+ */
+void free_array_of_pointers(char **array)
+{
+	int i;
+
+	if (array != NULL)
+	{
+		for (i = 0: array[1]; i++)
+			free(array[i]);
+
+		free(array);
+		array = NULL;
+	}
+}
